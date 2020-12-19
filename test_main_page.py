6132ac143ef6493b00ba5588721b72cd9f4a4be4
Alexander Prymak <primak.aa@gmail.com>
2020-12-19 06:07:29 +0300

    Fixed test_guest_can_go_to_login_page test. LoginPage initialization applied in test body

--- test_main_page.py
+++ test_main_page.py
@@ -16,8 +16,10 @@ def test_guest_should_see_login_link(browser):
     page.should_be_login_link()
 
 
-def test_guest_should_see_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
     page.open()
-    page.should_be_login_page()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
